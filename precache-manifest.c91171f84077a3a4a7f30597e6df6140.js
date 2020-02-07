655455f055fa9bd2d22807ae05c9ab7104ce824c
P Skolsky <patrickskolskyart@gmail.com>
2020-02-07 16:32:12 -0500

    Updates

---
+++ precache-manifest.c91171f84077a3a4a7f30597e6df6140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db09a5c30aff54122b317e3ead5ef13",
+    "url": "/click-game/index.html"
+  },
+  {
+    "revision": "e17522707aaf9b862502",
+    "url": "/click-game/static/css/main.9e528281.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/click-game/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/click-game/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e17522707aaf9b862502",
+    "url": "/click-game/static/js/main.1ab82339.chunk.js"
+  },
+  {
+    "revision": "e2801d1c9c1b8cfc7a8f",
+    "url": "/click-game/static/js/runtime-main.410183ed.js"
+  },
+  {
+    "revision": "eee679af6a9519e09f00cb2ae795030e",
+    "url": "/click-game/static/media/athf_bg.eee679af.jpeg"
+  }
+]);
