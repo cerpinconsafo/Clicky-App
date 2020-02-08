aa1f436f601987c0a07f1da5828c63b0590e5ef0
P Skolsky <patrickskolskyart@gmail.com>
2020-02-07 19:39:38 -0500

    Updates

---
+++ precache-manifest.0723dab83ac0cdf3143a25035c384ef5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053478ce38d504b63b1b08b6ba27ee1e",
+    "url": "/clicky-app/index.html"
+  },
+  {
+    "revision": "457d090072b50b213779",
+    "url": "/clicky-app/static/css/main.3ece6dad.chunk.css"
+  },
+  {
+    "revision": "92e68c3a1cfe74c9d40e",
+    "url": "/clicky-app/static/js/2.a1228723.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-app/static/js/2.a1228723.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "457d090072b50b213779",
+    "url": "/clicky-app/static/js/main.7779bdb3.chunk.js"
+  },
+  {
+    "revision": "5040c0652baca63085d7",
+    "url": "/clicky-app/static/js/runtime-main.d06158f2.js"
+  },
+  {
+    "revision": "eee679af6a9519e09f00cb2ae795030e",
+    "url": "/clicky-app/static/media/athf_bg.eee679af.jpeg"
+  }
+]);
